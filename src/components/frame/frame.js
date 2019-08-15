06a0919ee023a8a96a1ba730a93737cbb00e3113
Vladyslav Ganin <vladganin@gmail.com>
2019-08-15 21:50:32 +0200

    Updating frame.js
    Now color changes upon selecting respective field in option selection. Whooray

--- src/components/frame/frame.js
+++ src/components/frame/frame.js
@@ -3,15 +3,43 @@ import './frame.css';
 
 function ColorBox(props) {
   return (
-    <div className={props.color}></div>
+    <div className={props.valuepassed}></div>
   )
+}
+
+class WelcomeText extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 'black' };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className='welcometext' >
+        <ColorBox valuepassed={this.state.value} />
+        <h1>Hello!</h1>
+        <p>Please choose the color. It will change the box color on click.</p>
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option selected value="black">Black</option>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+        </select>
+      </div>
+    )
+  }
 }
 
 function Frame() {
   return (
     <div className={'box'}>
-      <ColorBox color="black" />
-      <h1>hello</h1>
+      <WelcomeText />
     </div>
   )
 }
